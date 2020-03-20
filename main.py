d5318f853a84d8aac39362e8ff2c644775f3af95
1cekrim <1cekrim.dev@gmail.com>
2020-03-20 21:02:42 +0900

    feat: Implement get_user_key, get_courses, anmd print_coutses

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-from bs4 import BeautifulSoup
+import json
 
 class HYUBlackboard:
     def __init__(self, **kwargs):
@@ -13,10 +13,35 @@ class HYUBlackboard:
         print(url)
         cookies = {'BbRouter': self.BbRouter}
         rep = self.session.get(url, cookies = cookies, verify=False)
-        print(rep.text)
-        soup = BeautifulSoup(rep.text)
-        lst = soup.findAll("id")
-        print(lst)
+        idx = rep.text.index('"id":') + 6
+        self.user_key = ''
+        while rep.text[idx] != '"':
+            self.user_key += rep.text[idx]
+            idx += 1
+        print(self.user_key)
 
-blackboard = HYUBlackboard(BbRouter='')
+    def get_courses(self):
+        url = self.url+ f'/learn/api/v1/users/{self.user_key}/memberships?expand=course.effectiveAvailability,course.permissions,courseRole&includeCount=true&limit=10000'
+        print(url)
+        cookies = {'BbRouter': self.BbRouter}
+        rep = self.session.get(url, cookies = cookies, verify=False)
+        self.courses = []
+        for course in json.loads(rep.text)['results']:
+            print(course)
+            course = course['course']
+            dic = {}
+            dic['name'] = course['name']
+            dic['id'] = course['id']
+            dic['courseId'] = course['courseId']
+            dic['term'] = course['term']['name'] if 'term' in course else 'None'
+            self.courses.append(dic)
+    
+    def print_courses(self):
+        for course in self.courses:
+            print(course, sep=' ')
+
+
+blackboard = HYUBlackboard(BbRouter=input())
 blackboard.get_user_key()
+blackboard.get_courses()
+blackboard.print_courses()
